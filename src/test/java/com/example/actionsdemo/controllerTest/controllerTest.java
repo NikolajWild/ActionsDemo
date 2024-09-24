03002d75eef213ecd150cdc65f94abc077e18b39
NikolajWild <nime0002@stud.kea.dkgit config --global user.email NikolajWildgit config --global user.email nime0002@stud.kea.dk>
2024-09-24 21:22:30 +0200

    25-27

---
+++ src/test/java/com/example/actionsdemo/controllerTest/controllerTest.java
@@ -0,0 +1,34 @@
+package com.example.actionsdemo.controllerTest;
+
+import com.example.actionsdemo.controller.WelcomeController;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(WelcomeController.class)
+public class controllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+    }
+    @AfterEach
+    void tearDown(){
+    }
+    @Test
+    public void testWelcome() throws Exception{
+        mockMvc.perform(get("/welcome"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("welcome"));
+    }
+
+}
